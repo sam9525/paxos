7c0c69666cf9f640998d7feabccf361060962d71
sam9525 <sam9985246327@gmail.com>
2024-11-18 15:13:16 +1030

    Create proposal class

---
+++ Proposal.java
@@ -0,0 +1,10 @@
+public class Proposal {
+
+  int proposalNumber;
+  String value;
+
+  public Proposal(int proposalNumber, String value) {
+    this.proposalNumber = proposalNumber;
+    this.value = value;
+  }
+}
